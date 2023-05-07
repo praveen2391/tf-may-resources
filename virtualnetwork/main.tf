5b0e9befc7db26cacb855c4eca047c02c8f64f83
Praveenkumar Narasimman <praveenkumar.narasimman@atos.net>
2023-05-07 07:36:31 +0530

    vn update

---
+++ virtualnetwork/main.tf
@@ -0,0 +1,15 @@
+resource "azurerm_virtual_network" "virtual_network" {
+  name                = var.vnet-name
+  location            = var.rg-location
+  resource_group_name = var.rg-name
+  /* location            = data.azurerm_resource_group.resource_group.location
+  resource_group_name = data.azurerm_resource_group.resource_group.name */
+  address_space = var.address-space
+  dns_servers   = var.dns-servers
+
+
+  tags = {
+    environment = var.environment
+  }
+
+}
---
+++ virtualnetwork/variables.tf
@@ -0,0 +1,26 @@
+variable "vnet-name" {
+  type    = string
+  default = "tfpoc-vnet"
+}
+
+variable "rg-location" {
+  type    = string
+  default = "West Europe"
+}
+
+variable "rg-name" {
+  type    = string
+  default = "tfpoc-rg"
+}
+variable "address-space" {
+  default = ["10.0.0.0/16"]
+}
+
+variable "dns-servers" {
+  default = ["10.0.0.4", "10.0.0.5"]
+}
+
+variable "environment" {
+  type    = string
+  default = "Dev"
+}
