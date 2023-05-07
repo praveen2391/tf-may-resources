8b48d24cd26847bdd90d57a2d93c714299f3ec09
Praveenkumar Narasimman <praveenkumar.narasimman@atos.net>
2023-05-07 07:41:42 +0530

    subnet update

---
+++ subnet/output.tf
@@ -0,0 +1,7 @@
+output "subnet-name" {
+  value = azurerm_subnet.subnet.name
+}
+
+output "subnet-id" {
+  value = azurerm_subnet.subnet.id
+}
---
+++ subnet/variables.tf
@@ -0,0 +1,18 @@
+variable "subnet-name" {
+  type    = string
+  default = "tfpoc-subnet"
+}
+
+variable "address-prefixes" {
+  default = ["10.0.1.0/24"]
+}
+
+variable "rg-name" {
+  type    = string
+  default = "tfpoc-rg"
+}
+
+variable "vnet-name" {
+  type    = string
+  default = "tfpoc-vnet"
+}
